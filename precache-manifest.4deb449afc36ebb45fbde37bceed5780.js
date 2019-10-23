77b9ee4e8a93a4c2b3a31dd500d718c39682c179
Chris Ruff <Chrisruff@live.ca>
2019-10-23 18:22:46 -0300

    Updates

---
+++ precache-manifest.4deb449afc36ebb45fbde37bceed5780.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59993f82f1f049a1e78fb9b8f30ef1fd",
+    "url": "/ReactJS_Stuff/index.html"
+  },
+  {
+    "revision": "d24db13fc3d02f1fa927",
+    "url": "/ReactJS_Stuff/static/css/main.7eb785de.chunk.css"
+  },
+  {
+    "revision": "990a30170cdff29bfc6f",
+    "url": "/ReactJS_Stuff/static/js/2.b1641691.chunk.js"
+  },
+  {
+    "revision": "d24db13fc3d02f1fa927",
+    "url": "/ReactJS_Stuff/static/js/main.1cf2a1e5.chunk.js"
+  },
+  {
+    "revision": "d5e3754bcdd23f3878c4",
+    "url": "/ReactJS_Stuff/static/js/runtime-main.275649fb.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/ReactJS_Stuff/static/media/logo.25bf045c.svg"
+  }
+]);
