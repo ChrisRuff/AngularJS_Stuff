c69651c80cff78d0e76b174e5fea36b3cec8f45c
Chris Ruff <Chrisruff@live.ca>
2019-10-24 09:25:37 -0300

    Working on Array Sorter, Using setState

--- projects/src/ArraySorter/ArraySorter.js
+++ projects/src/ArraySorter/ArraySorter.js
@@ -1,42 +1,44 @@
 import React from "react"
 import Rectangle from "react-rectangle";
-var data;
+import {useState} from "react";
+import itemStyle from "./ArrayStyle.css"
 function ArraySorter()
 {
-    if(data == null)
-        data = new ArrayModel();
-    return(data);
-}
+    const [data, setData] = useState(new ArrayModel());
 
+    function plus(data) {
+        data.data.push(listOfRectangles(0, 600, 1));
+        setData(data);
+    }
+
+
+    return(
+        <div>
+            <ul>
+                {data.data}
+            </ul>
+            <button onClick={() => plus(data)}>Add a Rectangle</button>
+        </div>
+
+    );
+}
 class ArrayModel extends React.Component
 {
     constructor()
     {
         super();
-        this.data = listOfRectangles(5, 10, 10);
+        this.data = listOfRectangles(5, 600, 10);
     }
     render()
     {
-        return ( <div>{this.data}</div> );
+        return (this.data);
     }
-    sort(array)
-    {
-        let timer = null;
-        function frame()
-        {
-            let tmp;
-            for (let i = 0; i < array.length; i++)
-            {
-                for (let j = 0; j < array.length; j++)
-                {
-                    tmp = this.data[i];
-                    this.data[i] = this.data[j];
-                    this.data[j] = tmp;
-                }
-            }
 
-        }
-        timer=setInterval(frame,300);
+    change(x, y)
+    {
+        let tmp = this.data[x];
+        this.data[x] = this.data[y];
+        this.data[y] = tmp;
     }
 }
 function listOfRectangles(x, y, num)
@@ -44,36 +46,26 @@ function listOfRectangles(x, y, num)
     let array = [];
     for(let i = 0; i < num; i++)
     {
+        let height = randomNum(0, y);
         array.push(
-
-            <li style={{float: "Left"}}>{rectangle(x,randomNum(0, 500))}</li>
+            <li style={{float: "Left", background: '#9e9e9e', width: x, height: height }}>{rectangle(x,height)}</li>
         );
     }
-    return (
-        <ul>
-            {array}
-        </ul>
-    );
+    return (array);
 
 
 
 }
-
 function rectangle(x, y)
 {
     return (
-        <div style={{ background: '#9e9e9e', width: x, height: y }}>
-            <Rectangle aspectRatio={[5, 3]}>
-                <div style={{ background: '#607d8b', width: '100%', height: '100%' }} />
-            </Rectangle>
-        </div>
+        <Rectangle aspectRatio={[5, 3]}>
+            <div style={{ background: '#607d8b', width: '100%', height: '100%' }} />
+        </Rectangle>
     );
 }
-
 function randomNum(min, max)
 {
     return (Math.random() * max) + min;
 }
-
-
 export default ArraySorter;
